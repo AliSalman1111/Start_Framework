dfad5032521bfac26bd64d965976b1a228e8d4d5
AliSalman444 <alisalman444111@gmail.com>
2026-01-09 05:20:12 +0200

    ..

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { Home } from "./componants/home/home";
+import { Navbar } from "./componants/navbar/navbar";
+import { Footer } from "./componants/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Home, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
