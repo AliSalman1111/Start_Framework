edf6e2e3c97394580973f8f5334a470e48b695d8
AliSalman444 <alisalman444111@gmail.com>
2026-01-09 15:14:02 +0200

    end about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path: '', redirectTo: 'home', pathMatch: 'full'},
+{path: 'home', loadComponent: () => import('./componants/home/home').then(m => m.Home)},
+{path: 'about', loadComponent: () => import('./componants/about/about').then(m => m.About)},
+
+{path: '**', redirectTo: 'home'
+
+}
+
+
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,12 @@ import { Component, signal } from '@angular/core';
 import { Home } from "./componants/home/home";
 import { Navbar } from "./componants/navbar/navbar";
 import { Footer } from "./componants/footer/footer";
+import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [Home, Navbar, Footer],
+  imports: [Home, Navbar, Footer, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
