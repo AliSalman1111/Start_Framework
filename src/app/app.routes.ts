3ade93b37bcfbb5951d1d464c3fd9339b3f59f1e
AliSalman444 <alisalman444111@gmail.com>
2026-04-08 19:33:03 +0200

    i finsh task

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,8 @@ export const routes: Routes = [
 {path: '', redirectTo: 'home', pathMatch: 'full'},
 {path: 'home', loadComponent: () => import('./componants/home/home').then(m => m.Home)},
 {path: 'about', loadComponent: () => import('./componants/about/about').then(m => m.About)},
-
+{path: 'portfolio', loadComponent: () => import('./componants/portfolio/portfolio').then(m => m.Portfolio)},
+{path: 'contact', loadComponent: () => import('./componants/contact/contact').then(m => m.Contact)},
 {path: '**', redirectTo: 'home'
 
 }
